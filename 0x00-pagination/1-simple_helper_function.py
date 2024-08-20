1968580c223c63d4f8fe9ddf8271c03c6a8e4025
Worlako Dzokoto <worlaco.dzo@gmail.com>
2024-08-20 04:23:50 +0000

    Added 1-simple-helper-function

---
+++ 0x00-pagination/1-simple_helper_function.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""
+This module provides a simple helper
+function to load index range of a page.
+"""
+
+import csv
+import math
+from typing import List
+
+
+def index_range(page, page_size):
+    """
+    Return a tuple that contains the
+    start and end index of a page.
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names."""
+
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert isinstance(page, int) and isinstance(
+            page_size, int
+        ), "raised when page and/or page_size are not ints"
+        assert page < 0 or page_size < 0, "raised with negative values"
+        assert page == 0 or page_size == 0, "raised with 0"
+
+        start_index, end_index = index_range(page, page_size)
+        return self.dataset()[start_index:end_index]
